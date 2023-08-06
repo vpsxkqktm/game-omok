a8583b28756a05de4310fb45814639afcd36c606
Jayyo <gracekim7765@gmail.com>
2023-08-06 17:14:34 +0900

    8월6일17시14분

--- src/App.js
+++ src/App.js
@@ -12,11 +12,12 @@ const App = () => {
 
   const handleClick = (i, j) => {
     if (board[i][j] !== EMPTY) return;
-    if (checkThreeThree(board, currentPlayer, i, j)) {
-      window.alert('3x3 is forbidden');
-      return;
-    }
     const newBoard = [...board];
+    newBoard[i][j] = currentPlayer;
+    if (checkThreeThree(newBoard, currentPlayer, i, j)) {
+      window.alert('삼삼이 상황이므로 돌을 놓을 수 없습니다.');
+      return;  // 돌을 두지 않음
+    }
     newBoard[i][j] = currentPlayer;
     setBoard(newBoard);
     if (checkWin(newBoard, currentPlayer, i, j)) {
@@ -51,29 +52,41 @@ const App = () => {
   }
 
   const checkThreeThree = (board, player, row, col) => {
-    if (player !== BLACK) return false;
+    if (player !== BLACK) return false;  // 삼삼은 흑돌에만 적용
     const directions = [[0, 1], [1, 0], [1, 1], [1, -1]];
     let threeCount = 0;
     for (let [dx, dy] of directions) {
-      let count = 0;
-      for (let i = -4; i <= 4; i++) {
-        const x = row + i * dx;
-        const y = col + i * dy;
-        if (x >= 0 && y >= 0 && x < BOARD_SIZE && y < BOARD_SIZE && board[x][y] === player) {
+      let count = 1; // 중심 돌을 포함
+      for (let i = 1; i <= 2; i++) { // 중심 돌의 양쪽 2개 셀 확인
+        const x1 = row + i * dx;
+        const y1 = col + i * dy;
+        const x2 = row - i * dx;
+        const y2 = col - i * dy;
+        if (x1 >= 0 && y1 >= 0 && x1 < BOARD_SIZE && y1 < BOARD_SIZE && board[x1][y1] === player) {
           count++;
-          if (count === 3 && 
-              x + dx >= 0 && y + dy >= 0 && x + dx < BOARD_SIZE && y + dy < BOARD_SIZE &&
-              x - 3 * dx >= 0 && y - 3 * dy >= 0 && x - 3 * dx < BOARD_SIZE && y - 3 * dy < BOARD_SIZE &&
-              board[x + dx][y + dy] === EMPTY && board[x - 3 * dx][y - 3 * dy] === EMPTY) {
-            threeCount++;
-          }
-        } else {
-          count = 0;
+        }
+        if (x2 >= 0 && y2 >= 0 && x2 < BOARD_SIZE && y2 < BOARD_SIZE && board[x2][y2] === player) {
+          count++;
+        }
+      }
+      if (count === 3) { // 돌이 3개인 경우
+        // 양쪽 끝 셀 확인
+        const x1 = row + 3 * dx;
+        const y1 = col + 3 * dy;
+        const x2 = row - 3 * dx;
+        const y2 = col - 3 * dy;
+        if (x1 >= 0 && y1 >= 0 && x1 < BOARD_SIZE && y1 < BOARD_SIZE && board[x1][y1] === EMPTY &&
+            x2 >= 0 && y2 >= 0 && x2 < BOARD_SIZE && y2 < BOARD_SIZE && board[x2][y2] === EMPTY) {
+          threeCount++;
         }
       }
     }
     return threeCount >= 2;
   }
+  
+  
+  
+  
 
   return (
     <div className="App">
